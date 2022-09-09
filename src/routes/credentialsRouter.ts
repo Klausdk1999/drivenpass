16c0d55df3c76a1b4cd6057b5c04f707a024358d
Klaus Kupper <klausdk1999@gmail.com>
2022-09-09 14:47:07 -0300

    feat: credentials

---
+++ src/routes/credentialsRouter.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import verifyToken from "../middlewares/validateToken.js";
+import * as credentialController from "../controllers/credentialsController.js";
+import { validateSchema } from "../middlewares/validateSchemaMiddleware";
+import credentialSchema from "../schemas/credentialSchema.js";
+
+const credentialsRouter = Router();
+
+credentialsRouter.get("/credentials", verifyToken, credentialController.getCredentialsFromUser);
+credentialsRouter.get("/credentials/:id",	verifyToken,credentialController.getCredentialById);
+credentialsRouter.post("/credentials", verifyToken,validateSchema(credentialSchema),credentialController.createCredential);
+credentialsRouter.delete("/credentials/:id", verifyToken,credentialController.deleteCredential);
+
+export default credentialsRouter;
